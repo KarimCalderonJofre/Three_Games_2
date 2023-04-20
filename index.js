3e110bd73a386adcde1cf45bd77f4fce4d792d7a
MatiasC22 <matias.calvar@gmail.com>
2023-04-20 16:04:27 -0300

    Codigo Limpio

--- index.js
+++ index.js
@@ -31,45 +31,8 @@ var intervalId = setInterval(cambiarImagen, 3000);
 imgBuscado.addEventListener("click", function() {
     clearInterval(intervalId);
 });
-/*var imgBuscado = document.getElementById("imagenJuegoB");
-var images = ["./assets/masBuscados/Tomb_Raider_1.webp", "./assets/masBuscados/Resident-Evil_2.jpg","./assets/masBuscados/roblox_3.jpg", "./assets/masBuscados/Fifa23_4.webp"];
-let i = 0;
-for (let i = 0; i < array.length; i++) {
-    
-
-    
-    setInterval(() => {
-        imgBuscado.src = images[i];
-    },3000)
-    
-    if(i===images.length){
-        i=0;
-    }
-    
-}
-    
 
 
-/*var btnNext = document.getElementById("next");
-var imgBuscado =document.getElementById("imagenJuegoB");
-
-
-var images = ["./assets/masBuscados/Tomb_Raider_1.webp", "./assets/masBuscados/Resident-Evil_2.jpg","./assets/masBuscados/roblox_3.jpg", "./assets/masBuscados/Fifa23_4.webp"]; 
-num = 0;
-
-btnNext.addEventListener("click", function(){
-    
-    num++;
-    if(num>=images.length){
-        num=0;
-        imgBuscado.src = images[num];
-    }else{
-        imgBuscado.src = images[num];
-    }
-        
-    
-});
-*/
 
 function validarFormulario() {
 			
